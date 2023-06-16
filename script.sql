02ac2e9e26258ad193a82353b44e5b3137a85326
Austin Lovold <austin@lovolds.com>
2023-06-16 09:17:32 -0500

    Fixed searches

--- script.sql
+++ script.sql
@@ -1,13 +1,13 @@
 USE bestbuy;
 SELECT * FROM products;
  
-SELECT * FROM  products WHERE products.Price = 1400.00;
+SELECT * FROM  products WHERE Price = 1400.00;
  
-SELECT * FROM products WHERE price = 11.99 OR p.price = 13.99;
+SELECT * FROM products WHERE price = 11.99 OR price = 13.99;
  
-SELECT * FROM products WHERE NOT p.price = 11.99;
+SELECT * FROM products WHERE NOT price = 11.99;
  
-SELECT * FROM products ORDER BY p.price DESC;
+SELECT * FROM products ORDER BY price DESC;
 
 SELECT * FROM employees WHERE MiddleInitial IS NULL;
  
@@ -15,7 +15,7 @@ SELECT DISTINCT price FROM products;
  
 SELECT * FROM employees WHERE FirstName LIKE 'j%';
  
-SELECT * FROM products WHERE NAME = 'Macbook%';
+SELECT * FROM products WHERE NAME LIKE 'Macbook%';
  
 SELECT * FROM products WHERE OnSale = 1;
  
